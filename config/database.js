29e07fccad60af54333112801791625c90d9722d
CodeMagician124 <101555018+CodeMagician124@users.noreply.github.com>
2023-02-12 04:24:30 +0900

    DB Config Update

--- config/database.js
+++ config/database.js
@@ -5,4 +5,10 @@ const db = new Sequelize.Sequelize('testdatabase', 'root', 'Aeritex124!@$iekoMed
   dialect: 'mysql'
 })
 
+db.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
+
 module.exports = db
