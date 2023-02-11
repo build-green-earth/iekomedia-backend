53292f047f441b3cf820a4b0a19b014ddff3dbd3
CodeMagician124 <101555018+CodeMagician124@users.noreply.github.com>
2023-02-11 22:23:02 +0900

    Db config update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const db = new Sequelize.Sequelize('testdatabase', 'root', '', {
+const db = new Sequelize.Sequelize('testdatabase', 'root', 'Aeritex124!@$iekoMedia', {
   host: 'localhost',
   dialect: 'mysql'
 })
