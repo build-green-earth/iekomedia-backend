69ed7e66ab9c8f1403699048715721acc94897c9
CodeMagician124 <101555018+CodeMagician124@users.noreply.github.com>
2023-02-27 19:01:58 +0900

    Schema Update

--- models/User.js
+++ models/User.js
@@ -7,16 +7,17 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  name: String,
   email: String,
   password: String,
+  firstName: String,
+  lastName: String,
   refreshToken: String,
   role: String,
   location: String,
   factory: String,
   approved: Boolean,
   admin: Boolean,
-  restrict: Boolean
+  restrict: Boolean,
 })
 
 module.exports = mongoose.model("Users", userSchema)
